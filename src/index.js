852f4960d8dc1651d90957a91b1666de69c1f5b3
Wangmingxing <starpentadream@gmail.com>
2018-07-17 15:49:05 +0800

    添加热更新的配置

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import App from './App'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import App from "./App";
+import { AppContainer } from "react-hot-loader";
+import "babel-polyfill";
 
-render(
-    <App></App>,
-    document.getElementById('app')
-);
+const renderDom = Component => {
+    render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        document.getElementById("app")
+    );
+};
+renderDom(App);
+
+if (module.hot) {
+    module.hot.accept("./App", () => {
+        const App = require("./App").default;
+        renderDom(App);
+    });
+}
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
     mode: 'development',
     devtool: 'cheap-module-eval-source-map',
-    entry: path.resolve(__dirname, "../src/index.js"), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+    entry: [
+        'babel-polyfill',
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:9090',
+        'webpack/hot/only-dev-server',
+        path.resolve(__dirname, "../src/index.js")], //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+    //添加热更新入口
     output: {
         path: path.resolve(__dirname, "../dist"), // 输出的路径
         filename: 'app/[name]_[hash:8].js', // 打包后文件
@@ -42,9 +49,12 @@ module.exports = {
         ]
     },
     plugins: [
+        //默认html模板
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../src/index.template.html'),
             inject: true
-        })
+        }),
+        //热更新
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
